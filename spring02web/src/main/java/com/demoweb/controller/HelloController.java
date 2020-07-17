f115360db08ccabf76556fd09ce8b45bee5cae55
hyh09 <455663056@qq.com>
2020-07-17 16:39:48 +0800

    提交一份 模板引擎;Test

--- spring02web/src/main/java/com/demoweb/controller/HelloController.java
+++ spring02web/src/main/java/com/demoweb/controller/HelloController.java
@@ -1,16 +1,18 @@
 package com.demoweb.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by dell on 2020/7/17.
  */
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/")
+
     public  String get1(){
-        return  "Hello World!!";
+        return  "init";
     }
 }
---
+++ spring02web/src/main/java/com/demoweb/dao/ne4j/entity/Movie.java
@@ -0,0 +1,11 @@
+package com.demoweb.dao.ne4j.entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+
+/**
+ * Created by dell on 2020/7/17.
+ */
+//@JsonIdentityInfo(generator = JSOGGenerator.class)
+
+public class Movie {
+}
