44ee1596e4f3c604230e10bafd37f09cecdc100c
hyh09 <455663056@qq.com>
2020-07-21 21:34:30 +0800

    java.lang.NullPointerException  ==hashTable 添加null会报空指针

--- spring02web/src/test/java/com/javatooltest/TestHah1.java
+++ spring02web/src/test/java/com/javatooltest/TestHah1.java
@@ -1,9 +1,28 @@
 package com.javatooltest;
 
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+
 /**
  * Created by dell on 2020/7/21.
  */
 public class TestHah1 {
 
     @Test
+    public  void Test1(){
+//        #hashMap允许空键值，而hashTable不允许
+        HashMap  hashMap = new HashMap();
+        hashMap.put(null,null);
+        hashMap.put("1","1");
+        System.out.println("====>"+hashMap);
+
+       Hashtable   hashTable = new Hashtable();
+       hashTable.put(null,null);
+       hashTable.put("1","1");
+        System.out.println("====>"+hashTable);
+
+
+    }
 }
