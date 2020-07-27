e9accc4545ec5dc98dd7e37f08f17bac08ffcbbb
hyh09 <455663056@qq.com>
2020-07-27 11:47:07 +0800

    java.lang.NullPointerException

--- spring02web/src/test/java/com/javatooltest/TestHah1.java
+++ spring02web/src/test/java/com/javatooltest/TestHah1.java
@@ -2,8 +2,8 @@ package com.javatooltest;
 
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Hashtable;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by dell on 2020/7/21.
@@ -24,5 +24,28 @@ public class TestHah1 {
         System.out.println("====>"+hashTable);
 
 
+    }
+
+
+
+    @Test
+    public  void Test12(){
+      //  List<Map<String, Object>> list = jdbcTemplate.queryForList(sql, prov);
+      System.out.print( genKey("41"));
+    }
+
+
+    private String genKey(String domain) {
+
+        StringBuilder keyBuilder = new StringBuilder("");
+        // 获得时间（8位），年月日（yyyyMMdd）
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        keyBuilder.append(sdf.format(new Date()));
+        // 调度者被执行的时间（2位）
+        keyBuilder.append("20");
+        // 省编码（2位）
+        keyBuilder.append(domain);
+
+        return keyBuilder.toString();
     }
 }
