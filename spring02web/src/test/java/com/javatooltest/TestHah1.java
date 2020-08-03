a54705f5947bc0d250ed98d1f4638e1b3919e54f
hyh09 <455663056@qq.com>
2020-08-03 22:08:10 +0800

    hello world

--- spring02web/src/test/java/com/javatooltest/TestHah1.java
+++ spring02web/src/test/java/com/javatooltest/TestHah1.java
@@ -9,43 +9,64 @@ import java.util.*;
  * Created by dell on 2020/7/21.
  */
 public class TestHah1 {
+    /**
+     * //评测题目: 请对指定的一段英文进行按单词进行逆转，
+     // 比如：Hello world！逆转结果为：world Hello！
+     // 又如：Hello everybody，I am just a simple example.
+     // 逆转后为：everybody Hello, example simple a just am I.
+     */
+
+    public static void main(String[] args) {
+
+        String str="hello world";
+
+        StringBuffer sbf=new StringBuffer();
+
+//字符串分割
+
+        String [] arr=str.split(" ");
+
+        for(int i=arr.length-1;i>=0;i--){
+
+            sbf.append(arr[i]+" ");
+
+        }
+
+        System.out.println(sbf.toString());
+
+    }
+
 
     @Test
-    public  void Test1(){
-//        #hashMap允许空键值，而hashTable不允许
-        HashMap  hashMap = new HashMap();
-        hashMap.put(null,null);
-        hashMap.put("1","1");
-        System.out.println("====>"+hashMap);
+    public  void Test1202(){
+        String  str1="Hello everybody,I am just a simple example ";
+        //1.先逗号分隔
+        String[] split = str1.split(",");
+        StringBuffer stringBuffer = new StringBuffer("");
+        System.out.println("===>"+Arrays.toString(split));
+        System.out.println("===>"+split.length);
 
-       Hashtable   hashTable = new Hashtable();
-       hashTable.put(null,null);
-       hashTable.put("1","1");
-        System.out.println("====>"+hashTable);
+        for(int i=0;i<split.length;i++){
 
+            stringBuffer.append(Test12(split[i]+""));
+        }
+        System.out.print("===>"+stringBuffer);
+//everybody Hello, example simple a just am I.
+    }
+
+    public  String Test12( String str){
+        System.out.println("入参"+str);
+       // String str="hello world";
+        StringBuffer sbf=new StringBuffer();
+        String [] arr=str.split(" ");
+        for(int i=arr.length-1;i>=0;i--){
+            sbf.append(arr[i]+" ");
+        }
+    System.out.println("===1>"+sbf.toString());
+        return sbf.toString();
 
     }
 
 
 
-    @Test
-    public  void Test12(){
-      //  List<Map<String, Object>> list = jdbcTemplate.queryForList(sql, prov);
-      System.out.print( genKey("41"));
-    }
-
-
-    private String genKey(String domain) {
-
-        StringBuilder keyBuilder = new StringBuilder("");
-        // 获得时间（8位），年月日（yyyyMMdd）
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-        keyBuilder.append(sdf.format(new Date()));
-        // 调度者被执行的时间（2位）
-        keyBuilder.append("20");
-        // 省编码（2位）
-        keyBuilder.append(domain);
-
-        return keyBuilder.toString();
-    }
 }
